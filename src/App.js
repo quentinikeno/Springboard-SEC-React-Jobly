b79148e3facce0dd8cf1f5a04757724359304df0
Quentin Ikeno <ikenoq@gmail.com>
2022-07-21 18:55:41 -1000

    Add /profile route.

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
 				<Route path="/jobs" element={<Jobs />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<Signup />} />
+				<Route path="/profile" element={<Profile />} />
 			</Routes>
 		</div>
 	);
