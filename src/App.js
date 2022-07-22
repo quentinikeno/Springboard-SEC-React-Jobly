36e3103b302c186e39ef7cfbe434cacb7249dc90
Quentin Ikeno <ikenoq@gmail.com>
2022-07-21 18:53:58 -1000

    Add /company/:handle route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage";
 import Companies from "./Companies";
+import CompanyDetails from "./CompanyDetails";
+import Jobs from "./Jobs";
+import Login from "./Login";
+import Signup from "./Signup";
+import Profile from "./Profile";
 import "./App.css";
 
 function App() {
