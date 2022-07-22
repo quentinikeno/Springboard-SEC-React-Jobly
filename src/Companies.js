898721c2edbfbecdcf2d756bd0568a80f8000947
Quentin Ikeno <ikenoq@gmail.com>
2022-07-22 11:37:17 -1000

    Add useEffect to fetch all companies data on Companies.

--- src/Companies.js
+++ src/Companies.js
@@ -1,6 +1,12 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import JoblyAPI from "./api_helpers/api";
 
 const Companies = () => {
+	const [companies, setCompanies] = useState([]);
+	useEffect(async function fetchCompaniesData() {
+		const data = await JoblyAPI.getCompanies();
+		setCompanies(data);
+	}, []);
 	return <div>Companies</div>;
 };
 
