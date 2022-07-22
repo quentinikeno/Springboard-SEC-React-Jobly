1b2156523b42e030ec82f61a372a7d1516a39099
Quentin Ikeno <ikenoq@gmail.com>
2022-07-22 10:30:28 -1000

    Add classes to style navbar.

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,13 +2,33 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
 	return (
-		<nav>
-			<NavLink to="/">Home</NavLink>
-			<NavLink to="/companies">Companies</NavLink>
-			<NavLink to="/jobs">Jobs</NavLink>
-			<NavLink to="/login">Login</NavLink>
-			<NavLink to="/signup">Sign Up</NavLink>
-			<NavLink to="/profile">Profile</NavLink>
+		<nav className="level">
+			<div className="level-left">
+				<span className="level-item is-size-2 has-text-primary">
+					Jobly
+				</span>
+				<NavLink className="level-item" to="/">
+					Home
+				</NavLink>
+				<NavLink className="level-item" to="/companies">
+					Companies
+				</NavLink>
+				<NavLink className="level-item" to="/jobs">
+					Jobs
+				</NavLink>
+			</div>
+
+			<div className="level-right">
+				<NavLink className="level-item" to="/login">
+					Login
+				</NavLink>
+				<NavLink className="level-item" to="/signup">
+					Sign Up
+				</NavLink>
+				<NavLink className="level-item" to="/profile">
+					Profile
+				</NavLink>
+			</div>
 		</nav>
 	);
 };
