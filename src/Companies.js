55d88c7a32630c1f872281adf38841cc13071831
Quentin Ikeno <ikenoq@gmail.com>
2022-07-22 11:47:27 -1000

    Render companyCards for each company.

--- src/Companies.js
+++ src/Companies.js
@@ -1,13 +1,27 @@
 import { useState, useEffect } from "react";
+import CompanyCard from "./CompanyCard";
 import JoblyAPI from "./api_helpers/api";
 
 const Companies = () => {
 	const [companies, setCompanies] = useState([]);
-	useEffect(async function fetchCompaniesData() {
-		const data = await JoblyAPI.getCompanies();
-		setCompanies(data);
+	useEffect(function fetchCompaniesDataOnLoad() {
+		async function fetchCompaniesData() {
+			const data = await JoblyAPI.getCompanies();
+			setCompanies(data);
+		}
+		fetchCompaniesData();
 	}, []);
-	return <div>Companies</div>;
+
+	const companyCards = companies.map((company) => (
+		<CompanyCard
+			key={company.handle}
+			handle={company.handle}
+			description={company.description}
+			logoUrl={company.logoUrl}
+		/>
+	));
+
+	return <div>{companyCards}</div>;
 };
 
 export default Companies;
