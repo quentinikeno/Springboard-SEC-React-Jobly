f138d20dfa47b8e31cbdd9823cf778606ef64182
Quentin Ikeno <ikenoq@gmail.com>
2022-07-22 15:39:33 -1000

    Implement Search and useEffect to show jobs on the page.

--- src/Jobs.js
+++ src/Jobs.js
@@ -1,7 +1,44 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import JobCard from "./JobCard";
+import Search from "./Search";
+import JoblyAPI from "./api_helpers/api";
 
 const Jobs = () => {
-	return <div>Jobs</div>;
+	const [jobs, setJobs] = useState([]);
+	const [jobQuery, setJobQueries] = useState(null);
+
+	useEffect(
+		function fecthJobsDataOnLoad() {
+			async function fetchJobsData() {
+				const data = await JoblyAPI.getJobs({ title: jobQuery });
+				setJobs(data);
+			}
+			fetchJobsData();
+		},
+		[jobQuery]
+	);
+
+	const search = (query) => {
+		query === "" ? setJobQueries(null) : setJobQueries(query);
+	};
+
+	const jobCards = jobs.map((job) => (
+		<JobCard
+			key={job.id}
+			title={job.title}
+			id={job.id}
+			salary={job.salary}
+			equity={job.equity}
+		/>
+	));
+
+	return (
+		<div>
+			<Search search={search} />
+			<section>{jobCards}</section>
+		</div>
+	);
 };
 
 export default Jobs;
