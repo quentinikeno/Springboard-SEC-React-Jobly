19ddf943ba9f216c58aac0f714040b5fcd6b6c54
Quentin Ikeno <ikenoq@gmail.com>
2022-07-22 15:10:40 -1000

    Take job data and render JobCards for each job.

--- src/CompanyDetail.js
+++ src/CompanyDetail.js
@@ -1,26 +1,31 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import JobCard from "./JobCard";
 import JoblyAPI from "./api_helpers/api";
 
 const CompanyDetail = () => {
 	const { handle } = useParams();
 	const [company, setCompany] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(function fetchCompanyDataOnLoad() {
 		async function fetchCompanyData() {
 			const data = await JoblyAPI.getCompany(handle);
 			setCompany(data);
+			setIsLoading(false);
 		}
 		fetchCompanyData();
 	}, []);
 
-	return (
+	return isLoading ? (
+		<p>Loading...</p>
+	) : (
 		<div>
 			<section className="hero is-info">
-				<div className="hero-body">
-					<h1 className="title">{company.name}</h1>
+				<div className="hero-body has-text-centered">
+					<h1 className="is-size-1">{company.name}</h1>
 					{company.logoUrl && (
-						<figure class="image is-96x96 mx-auto">
+						<figure className="image is-96x96 mx-auto">
 							<img src={company.logoUrl} />
 						</figure>
 					)}
@@ -29,6 +34,21 @@ const CompanyDetail = () => {
 						Number of employees: {company.numEmployees}
 					</p>
 				</div>
+			</section>
+			<section>
+				<h2 className="is-size-2 my-3 has-text-centered">
+					Job Openings:
+				</h2>
+				<hr />
+				{company.jobs.map((job) => (
+					<JobCard
+						key={job.id}
+						title={job.title}
+						id={job.id}
+						salary={job.salary}
+						equity={job.equity}
+					/>
+				))}
 			</section>
 		</div>
 	);
