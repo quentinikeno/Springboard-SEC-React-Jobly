78ad4de81d4a47022bb131272917ff84f6e54057
Quentin Ikeno <ikenoq@gmail.com>
2022-07-22 14:49:09 -1000

    Display basic company info on CompanyDetail.

--- src/CompanyDetail.js
+++ src/CompanyDetail.js
@@ -1,7 +1,37 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import JoblyAPI from "./api_helpers/api";
 
 const CompanyDetail = () => {
-	return <div>CompanyDetail</div>;
+	const { handle } = useParams();
+	const [company, setCompany] = useState([]);
+
+	useEffect(function fetchCompanyDataOnLoad() {
+		async function fetchCompanyData() {
+			const data = await JoblyAPI.getCompany(handle);
+			setCompany(data);
+		}
+		fetchCompanyData();
+	}, []);
+
+	return (
+		<div>
+			<section className="hero is-info">
+				<div className="hero-body">
+					<h1 className="title">{company.name}</h1>
+					{company.logoUrl && (
+						<figure class="image is-96x96 mx-auto">
+							<img src={company.logoUrl} />
+						</figure>
+					)}
+					<p className="subtitle">{company.description}</p>
+					<p className="subtitle">
+						Number of employees: {company.numEmployees}
+					</p>
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default CompanyDetail;
