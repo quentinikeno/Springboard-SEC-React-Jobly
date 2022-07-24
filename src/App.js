4884b797c9cda8df55918eecdba1cd32f36f1f6a
Quentin Ikeno <ikenoq@gmail.com>
2022-07-24 12:01:23 -1000

    Fix import to import from react.

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
 
 	return (
 		<div className="App container">
-			<UserContext.Provider value={user}>
+			<UserContext.Provider value={currentUser}>
 				<Navbar signOut={signOut} />
 				<Routes>
 					<Route path="/" element={<Homepage />} />
