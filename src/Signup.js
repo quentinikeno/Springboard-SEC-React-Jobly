6ea211e96bae3497e782dd0de56199e9794de664
Quentin Ikeno <ikenoq@gmail.com>
2022-07-24 10:32:57 -1000

    Add component with form to sign up.

--- src/Signup.js
+++ src/Signup.js
@@ -1,7 +1,111 @@
-import React from "react";
+import useFormState from "./custom_hooks/useFormState";
+import { useNavigate } from "react-router-dom";
 
-const Signup = () => {
-	return <div>Signup</div>;
+const Signup = ({ register }) => {
+	const initialState = {
+		username: "",
+		password: "",
+		firstName: "",
+		lastName: "",
+		email: "",
+	};
+	const [formData, setFormData, formErrors, setFormErrors, handleChange] =
+		useFormState(initialState);
+	const navigate = useNavigate();
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const res = await register(formData);
+		res.success ? navigate("/companies") : setFormErrors(res.errors);
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			{formErrors.length > 0 && (
+				<p className="help is-danger">{formErrors}</p>
+			)}
+			<div className="field">
+				<label className="label" htmlFor="username">
+					Username
+				</label>
+				<input
+					type="text"
+					className={`input ${
+						formErrors.length > 0 ? "is-danger" : ""
+					}`}
+					placeholder="username"
+					id="username"
+					name="username"
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="field">
+				<label className="label" htmlFor="password">
+					Password
+				</label>
+				<input
+					type="text"
+					className={`input ${
+						formErrors.length > 0 ? "is-danger" : ""
+					}`}
+					placeholder="password"
+					id="password"
+					name="password"
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="field">
+				<label className="label" htmlFor="firstName">
+					First Name
+				</label>
+				<input
+					type="text"
+					className={`input ${
+						formErrors.length > 0 ? "is-danger" : ""
+					}`}
+					placeholder="First Name"
+					id="firstName"
+					name="firstName"
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="field">
+				<label className="label" htmlFor="lastName">
+					Last Name
+				</label>
+				<input
+					type="text"
+					className={`input ${
+						formErrors.length > 0 ? "is-danger" : ""
+					}`}
+					placeholder="Last Name"
+					id="lastName"
+					name="lastName"
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="field">
+				<label className="label" htmlFor="email">
+					Email
+				</label>
+				<input
+					type="text"
+					className={`input ${
+						formErrors.length > 0 ? "is-danger" : ""
+					}`}
+					placeholder="Email"
+					id="email"
+					name="email"
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="field">
+				<button className="button is-link" type="submit">
+					Sign Up
+				</button>
+			</div>
+		</form>
+	);
 };
 
 export default Signup;
