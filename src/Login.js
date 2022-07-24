c03bbd76d3f50a926d3032489ab707e997d890a2
Quentin Ikeno <ikenoq@gmail.com>
2022-07-23 18:02:49 -1000

    Create login form.

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,56 @@
-import React from "react";
+import useFormState from "./custom_hooks/useFormState";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-	return <div>Login</div>;
+const Login = ({ login }) => {
+	const initialState = { username: "", password: "" };
+	const [formData, setFormData, handleChange] = useFormState(initialState);
+	const navigate = useNavigate();
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			await login(formData);
+			navigate("/");
+		} catch (error) {
+			alert(error);
+		}
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<div className="field">
+				<label className="label" htmlFor="username">
+					Username
+				</label>
+				<input
+					type="text"
+					className="input"
+					placeholder="username"
+					id="username"
+					name="username"
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="field">
+				<label className="label" htmlFor="password">
+					Password
+				</label>
+				<input
+					type="text"
+					className="input"
+					placeholder="password"
+					id="password"
+					name="password"
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="field">
+				<button className="button" type="submit">
+					Login
+				</button>
+			</div>
+		</form>
+	);
 };
 
 export default Login;
