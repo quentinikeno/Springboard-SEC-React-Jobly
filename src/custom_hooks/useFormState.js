1f70a7ed06a2978440df835e0e224e92199cb001
Quentin Ikeno <ikenoq@gmail.com>
2022-07-23 18:02:59 -1000

    Fix typo with event.target

--- src/custom_hooks/useFormState.js
+++ src/custom_hooks/useFormState.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const useFormState = (initialState) => {
 	const [formData, setFormData] = useState(initialState);
 	const handleChange = (event) => {
-		const { name, value } = event.target.value;
+		const { name, value } = event.target;
 		setFormData((formData) => ({ ...formData, [name]: value }));
 	};
 
