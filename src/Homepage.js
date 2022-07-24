2acd623dd2e3942c111241a591cf747d9d376f9c
Quentin Ikeno <ikenoq@gmail.com>
2022-07-24 12:30:06 -1000

    Create homepage component that shows different messages when a user is logged in.

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,7 +1,48 @@
-import React from "react";
+import { useContext } from "react";
+import UserContext from "./context/userContext";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
-	return <div>Homepage</div>;
+	const user = useContext(UserContext);
+	const subtitle = user ? (
+		<>
+			<p className="subtitle">
+				Welcome {user.username}! Get started by clicking a button below
+				to see jobs or companies.
+			</p>
+			<div className="buttons">
+				<button className="button is-link">
+					<Link to="/jobs">Jobs</Link>
+				</button>
+				<button className="button is-link is-light">
+					<Link to="/companies">Companies</Link>
+				</button>
+			</div>
+		</>
+	) : (
+		<>
+			<p className="subtitle">
+				Welcome to Jobly! Get started by signing in or creating a
+				account.
+			</p>
+			<div className="buttons">
+				<button className="button is-link">
+					<Link to="/login">Login</Link>
+				</button>
+				<button className="button is-link is-light">
+					<Link to="/signup">Sign Up</Link>
+				</button>
+			</div>
+		</>
+	);
+	return (
+		<section className="hero is-primary is-large">
+			<div className="hero-body">
+				<h1 className="title is-1">Jobly!</h1>
+				{subtitle}
+			</div>
+		</section>
+	);
 };
 
 export default Homepage;
