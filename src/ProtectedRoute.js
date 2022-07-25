6a6e5dfb971d1544ea4f9614cd1379a8717acb2d
Quentin Ikeno <ikenoq@gmail.com>
2022-07-24 14:58:28 -1000

    Create a component for protected routes.

---
+++ src/ProtectedRoute.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import UserContext from "./context/userContext";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+	const currentUser = useContext(UserContext);
+	return currentUser ? children : <Navigate to="/" />;
+};
+
+export default ProtectedRoute;
