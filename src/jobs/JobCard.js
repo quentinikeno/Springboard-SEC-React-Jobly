9829e4b59657f1cee6772316cd946fb8f3939cdb
Quentin Ikeno <ikenoq@gmail.com>
2022-07-25 17:59:17 -1000

    Add a button to apply for jobs and handle the click.

--- src/jobs/JobCard.js
+++ src/jobs/JobCard.js
@@ -1,24 +1,43 @@
-import React from "react";
+import { useContext } from "react";
+import UserContext from "../context/userContext";
 
 const JobCard = ({ id, title, salary, equity }) => {
+	const { currentUser, appliedJobs, applyOnClick } = useContext(UserContext);
+	const handleClick = () => {
+		applyOnClick(currentUser.username, id);
+	};
+	const applyJobButton =
+		appliedJobs.indexOf(id) === -1 ? (
+			<button
+				className="button is-primary is-light card-footer-item"
+				onClick={handleClick}
+			>
+				Apply Now
+			</button>
+		) : (
+			<button
+				className="button is-danger is-light card-footer-item"
+				disabled
+			>
+				Applied
+			</button>
+		);
 	return (
-		<div className="card mb-3">
-			<header className="card-header">
-				<p className="card-header-title title">{title}</p>
-			</header>
-			<div className="card-content">
-				<div className="content">
-					<ul>
-						<li>Salary: {salary}</li>
-						<li>Equity: {equity}</li>
-					</ul>
+		<div className="column is-full-mobile is-half-tablet mx-auto">
+			<div className="card mb-3 ">
+				<header className="card-header">
+					<p className="card-header-title title">{title}</p>
+				</header>
+				<div className="card-content">
+					<div className="content">
+						<ul>
+							<li>Salary: {salary}</li>
+							<li>Equity: {equity}</li>
+						</ul>
+					</div>
 				</div>
+				<footer className="card-footer">{applyJobButton}</footer>
 			</div>
-			<footer className="card-footer">
-				<a href="#" className="card-footer-item">
-					Save
-				</a>
-			</footer>
 		</div>
 	);
 };
