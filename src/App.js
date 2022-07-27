ee24d198b8e0d32dbdc0a6127d6ec6dbd10e2a7a
Quentin Ikeno <ikenoq@gmail.com>
2022-07-26 18:48:36 -1000

    Move components related to routes into their own folder.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import { useEffect, useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Homepage from "./Homepage";
-import Companies from "./companies/Companies";
-import CompanyDetail from "./companies/CompanyDetail";
-import Jobs from "./jobs/Jobs";
-import Login from "./auth_components/Login";
-import Signup from "./auth_components/Signup";
-import Profile from "./Profile";
-import Navbar from "./Navbar";
+import AppRoutes from "./routes/AppRoutes";
+import Navbar from "./routes/Navbar";
 import Loading from "./common/Loading";
-import ProtectedRoute from "./ProtectedRoute";
 import JoblyAPI from "./api_helpers/api";
 import UserContext from "./context/userContext";
 import useLocalStorageState from "./custom_hooks/useLocalStorageState";
@@ -105,47 +97,7 @@ function App() {
 				}}
 			>
 				<Navbar signOut={signOut} />
-				<Routes>
-					<Route path="/" element={<Homepage />} />
-					<Route
-						path="/companies"
-						element={
-							<ProtectedRoute>
-								<Companies />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path="/companies/:handle"
-						element={
-							<ProtectedRoute>
-								<CompanyDetail />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path="/jobs"
-						element={
-							<ProtectedRoute>
-								<Jobs />
-							</ProtectedRoute>
-						}
-					/>
-					<Route path="/login" element={<Login login={login} />} />
-					<Route
-						path="/signup"
-						element={<Signup register={register} />}
-					/>
-					<Route
-						path="/profile"
-						element={
-							<ProtectedRoute>
-								<Profile patch={patch} />
-							</ProtectedRoute>
-						}
-					/>
-					<Route path="*" element={<Navigate to="/" replace />} />
-				</Routes>
+				<AppRoutes login={login} register={register} patch={patch} />
 			</UserContext.Provider>
 		</div>
 	);
--- src/routes/Navbar.js
+++ src/routes/Navbar.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import UserContext from "./context/userContext";
+import UserContext from "../context/userContext";
 import { NavLink, Link } from "react-router-dom";
 
 const Navbar = ({ signOut }) => {
