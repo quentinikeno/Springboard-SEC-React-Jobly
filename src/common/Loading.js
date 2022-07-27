1d9ab979fd71ed05e2f9dcb4301cea2a2f32556d
Quentin Ikeno <ikenoq@gmail.com>
2022-07-26 14:01:38 -1000

    Create loading messsage component.

---
+++ src/common/Loading.js
@@ -0,0 +1,12 @@
+const Loading = () => {
+	return (
+		<div>
+			Loading...{" "}
+			<progress class="progress is-small is-primary" max="100">
+				15%
+			</progress>
+		</div>
+	);
+};
+
+export default Loading;
